0ec635cdc7e7cf98f354efd20e2a878d91e493b1
AmySeo12 <maryori.amy@gmail.com>
2016-11-03 22:52:05 -0500

    inicio copn fb

--- js/app.js
+++ js/app.js
@@ -1,41 +1,39 @@
 var cargar= function(){
-	$("#registrar").click(guardarSrc);
+	//$("#registrar").click(guardarSrc);
 
 	//API Facebook
-	  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '620589191445726',
-      xfbml      : true,
-      version    : 'v2.8'
-    });
-  };
+	window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '620589191445726',
+	      xfbml      : true,
+	      version    : 'v2.8'
+	    });
+	};
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+	(function(d, s, id)
+	  {
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }
+	   (document, 'script', 'facebook-jssdk')
+	);
+}
+
 
 	//--------------------------------------------------------
 
-	FB.ui(
- {
-  method: 'share',
-  href: 'https://developers.facebook.com/docs/'
-}, function(response){});
-}
-
 $(document).ready(cargar);
-var url= localStorage.getItem("url");
-var guardarSrc= function(e){
+//var url= localStorage.getItem("url");
+/*var guardarSrc= function(e){
 	e.preventDefault();
-	$(".optionsRadios").each(recorrer);
+	//$(".optionsRadios").each(recorrer);
 }
 
 var recorrer=function(){
 	if($(this).is(":checked")){
 		url = localStorage.setItem("url",$(this).next().attr("src"));
 	}
-}
+}*/
