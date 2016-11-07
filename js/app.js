2fad7326e31497225b056a5a9603165470ba3f53
AmySeo12 <maryori.amy@gmail.com>
2016-11-07 12:30:16 -0500

    a

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
 var cargar= function(){
 	//$("#registrar").click(guardarSrc);
+	function statusChangeCallback(response) {
+		if (response.status === 'connected') {
+			loginAPI();
+			friendAPI();
+		} else if (response.status === 'not_authorized') {
+			document.getElementById('user-picture').innerHTML = '';
+			document.getElementById('user-name').innerHTML = '';
+			document.getElementById('user-friends').innerHTML = '';
+			document.getElementById('status').innerHTML = 'Por favor, inicie sesión ' + 'en esta aplicación.';
+		} else {
+			document.getElementById('user-picture').innerHTML = '';
+			document.getElementById('user-name').innerHTML = '';
+			document.getElementById('user-friends').innerHTML = '';
+
+			document.getElementById('status').innerHTML = 'Por favor, inicie sesion' + 'en Facebook.';
+		}
+	}
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId      : '1850360375199136',
+			xfbml      : true,
+			version    : 'v2.8'
+		});
+	};
+
+	function checkLoginState() {
+		FB.getLoginStatus(function (response) {
+			statusChangeCallback(response);
+		});
+	}
 
 	(function(d, s, id) {
 		var js, fjs = d.getElementsByTagName(s)[0];
