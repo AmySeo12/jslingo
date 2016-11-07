3afffb2f256af051a9f6c7d1862859c82b4bded3
AmySeo12 <maryori.amy@gmail.com>
2016-11-07 10:52:07 -0500

    a

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 var cargar= function(){
 	//$("#registrar").click(guardarSrc);
+	(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.8&appId=620589191445726";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+	//API Facebook
+	window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '620589191445726',
+	      xfbml      : true,
+	      version    : 'v2.8'
+	    });
+	};
 
 	(function(d, s, id)
 	  {
@@ -11,23 +27,6 @@ var cargar= function(){
 	   }
 	   (document, 'script', 'facebook-jssdk')
 	);
-	//(function(d, s, id) {
-	//	var js, fjs = d.getElementsByTagName(s)[0];
-	//	if (d.getElementById(id)) return;
-	//	js = d.createElement(s); js.id = id;
-	//	js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.8&appId=620589191445726";
-	//	fjs.parentNode.insertBefore(js, fjs);
-	//}(document, 'script', 'facebook-jssdk'));
-
-	//API Facebook
-	window.fbAsyncInit = function() {
-	    FB.init({
-	      appId      : '620589191445726',
-	      xfbml      : true,
-	      version    : 'v2.8'
-	    });
-	};
-
 }
 
 
