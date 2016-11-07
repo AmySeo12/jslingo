f86f7b7b8f629c2d79e7f474143ccbb878e1774d
AmySeo12 <maryori.amy@gmail.com>
2016-11-07 12:32:21 -0500

    a

--- js/app.js
+++ js/app.js
@@ -61,6 +61,44 @@ var cargar= function(){
 	   }
 	   (document, 'script', 'facebook-jssdk')
 	);
+
+	function loginAPI() {
+		console.log('Bienvenido!  Buscamos su informacion .... ');
+		FB.api('/me', function (response) {
+			document.getElementById('status').innerHTML = 'Hola, ' + response.name + '<br>' + 'Bienvenido a Tinder';
+			document.getElementById('user-picture').innerHTML = '<img src="https://graph.facebook.com/' + response.id + '/picture">';
+			window.localStorage.setItem("idImg",response.id);
+		});
+	}
+
+	function friendAPI() {
+		document.getElementById('user-friends').innerHTML = 'before';
+		FB.api('me/friends?fields=id', function (response) {
+			if (!response.error) {
+				var markup = 'Amigos que usan esta app: ';
+				var friends = response.data;
+				var a = {};
+				for (var i = 0; i < friends.length && i < 25; i++) {
+					var friend = friends[i];
+					//markup += '<br>'+'<img src="' + friend.picture + '"> '+ '<br>' + friend.name;
+					markup += '<img src="https://graph.facebook.com/' + friend.id + '/picture">';
+					a[i] = friend.id;
+				}
+/*				var a = [];
+				for (var i = 0; i < friends.length && i < 25; i++) {
+					var friend = friends[i];
+					//markup += '<br>'+'<img src="' + friend.picture + '"> '+ '<br>' + friend.name;
+					markup += '<img src="https://graph.facebook.com/' + friend.id + '/picture">';
+					a.push(markup);
+				}*/
+				window.localStorage.setItem("array", JSON.stringify(a));
+				document.getElementById('user-friends').innerHTML = markup;
+			} else {
+				document.getElementById('user-friends').innerHTML = 'after';
+			}
+		});
+	}
+	checkLoginState();
 }
 
 
