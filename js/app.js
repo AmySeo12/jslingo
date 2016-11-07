cce79fa2c0a90406c0c3619b413464badc0d3c6f
AmySeo12 <maryori.amy@gmail.com>
2016-11-07 10:49:50 -0500

    ja

--- js/app.js
+++ js/app.js
@@ -1,15 +1,6 @@
 var cargar= function(){
 	//$("#registrar").click(guardarSrc);
 
-	//API Facebook
-	window.fbAsyncInit = function() {
-	    FB.init({
-	      appId      : '620589191445726',
-	      xfbml      : true,
-	      version    : 'v2.8'
-	    });
-	};
-
 	(function(d, s, id)
 	  {
 	     var js, fjs = d.getElementsByTagName(s)[0];
@@ -20,6 +11,23 @@ var cargar= function(){
 	   }
 	   (document, 'script', 'facebook-jssdk')
 	);
+	//(function(d, s, id) {
+	//	var js, fjs = d.getElementsByTagName(s)[0];
+	//	if (d.getElementById(id)) return;
+	//	js = d.createElement(s); js.id = id;
+	//	js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.8&appId=620589191445726";
+	//	fjs.parentNode.insertBefore(js, fjs);
+	//}(document, 'script', 'facebook-jssdk'));
+
+	//API Facebook
+	window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '620589191445726',
+	      xfbml      : true,
+	      version    : 'v2.8'
+	    });
+	};
+
 }
 
 
